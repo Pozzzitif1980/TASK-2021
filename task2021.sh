d1a4b705bfad66d11c717869f66a2b9ecd785e37
Pozzzitif1980 <92913891+Pozzzitif1980@users.noreply.github.com>
2021-12-07 15:13:52 +0200

    Add files via upload

---
+++ task2021.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo 'author - pozzzzitif1980'
+ps -eo pcpu | sed 1d |  awk '{s+=$0}END{print "cpu:" s "%"}'
+ 
+
+df -h | awk '$NF=="/"{printf "free_disk_space: %d/%dGB (%s)\n", $3,$2,$5}'
+
+
+D=$(date  +%Y-%m-%d)
+T=$(date +%H:%M:%S)
+echo  "generated_date:"  "$D" "$T"
+
+
